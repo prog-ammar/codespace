c040f7bcdc32652190b28c6a80ac8422fa0b99f3
prog-ammar <ammarofc2005@gmail.com>
2025-01-04 22:38:30 +0500

    Added

--- Finals/1darrayrev.cpp
+++ Finals/1darrayrev.cpp
@@ -2,11 +2,26 @@
 
 using namespace std;
 
+void rev(int* p, int n)
+{
+    int temp;
+    for(int i=0;i<n/2;i++)
+    {
+      temp=*(p+i);
+      *(p+i)=*(p+n-i-1);
+      *(p+n-i-1)=temp;
+    }
+}
+
 int main()
 {
     int n;
     cout<<"Enter no of Elements : ";
     cin>>n;
     int arr[n];
-    for(int i=0;i<n)
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    rev(arr,n);
 }
