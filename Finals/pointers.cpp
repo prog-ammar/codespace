ad67f07cd0003927a94866db95a8b7a854c2a715
prog-ammar <ammarofc2005@gmail.com>
2025-01-04 23:12:36 +0500

    Updated

--- Finals/pointers.cpp
+++ Finals/pointers.cpp
@@ -5,7 +5,43 @@ using namespace std;
 
 int main()
 {
- int x[7]={1,2,3,4,5,6,7};
- int* ptr= x;
- cout<<*ptr<<endl;
+//  int x[7]={1,3,4,5,6,7,8};
+//  int* ptr= x;
+//  ptr+=3;
+//  cout<<*ptr<<endl;
+
+
+//  int arr[3]={4,5,6};
+//  for(int i=0;i<3;i++)
+//  {
+//     cout<<*(arr+i)<<" ";
+//  }
+//  cout<<"\n";
+
+// int arr[] = {1, 3, 5, 7};
+//     int *ptr = arr + 1;
+//     cout << ptr[0] << " " << *(arr + 3) << endl;
+
+// int vals[] = {9, 18, 27, 36};
+//     int *ptr = vals;
+//     ptr += 2;
+//     cout << *(ptr + 1) << " " << *(ptr - 2) << endl;
+
+// int nums[] = {100, 200, 300};
+//     int *ptr1 = &nums[0];
+//     int *ptr2 = ptr1 + 1;
+//     cout << *ptr1 << " " << *(ptr2 + 1) << endl;
+
+// int arr[] = {10, 20, 30, 40, 50};
+//     int *ptr = arr;
+//     cout << *ptr + 2 << endl;      // Statement 1
+//     cout << *(ptr + 3) << endl;    // Statement 2
+//     cout << *(ptr + 1) - 5 << endl; // Statement 3
+
+int x=9;
+int* ptr1 =&x;
+int** ptr2 =&ptr1;
+int*** ptr3 =&ptr2;
+cout<<&x<<" "<<*ptr1<<" "<<**ptr2<<" "<<***ptr3;
+
 }
