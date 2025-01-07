38b09268f10e84b14fb01c1d2764b7a04efc2d3a
prog-ammar <ammarofc2005@gmail.com>
2025-01-07 18:28:35 +0500

    updated

--- Finals/2arraysmerging.cpp
+++ Finals/2arraysmerging.cpp
@@ -2,6 +2,57 @@
 
 using namespace std;
 
+int merging(int* arr1,int* arr2,int* arr3,int n1,int n2)
+{
+  int l=n1;
+    for(int i=0;i<n1;i++)
+    {
+        arr3[i]=arr1[i];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        bool found=false;
+        for(int j=0;j<n1;j++)
+        {
+           if(arr2[i]==arr1[j])
+           {
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+            {
+                arr3[l]=arr2[i];
+                l++;
+            }
+        }
+        return l;
+}
+
+void printarr(int* arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void sorting(int* p,int size)
+{
+    for(int i=1;i<size;i++)
+    {
+        for(int j=i;j>0;j--)
+        {
+           if(p[j-1]>p[j])
+           {
+            swap(p[j-1],p[j]);
+           }
+        }
+    }
+}
+
+
 
 int main()
 {
@@ -23,31 +74,10 @@ int main()
         cin>>arr2[i];
     }
     int arr3[n1+n2];
-    int l=n1;
-    for(int i=0;i<n1;i++)
-    {
-        arr3[i]=arr1[i];
-    }
-    for(int i=0;i<n2;i++)
-        {
-            bool found=false;
-            for(int j=i+1;j<n1;j++)
-            {
-              if(arr2[i]==arr1[j])
-              {
-                found=true;
-                break;
-              }
-            }
-            if(!found)
-            {
-                arr3[l]=arr2[i];
-                l++;
-            }
-        }
-    for(int i=0;i<l;i++)
-    {
-        cout<<arr3[i]<<" ";
-    }
-    cout<<endl;
+    int size=merging(arr1,arr2,arr3,n1,n2);
+    cout<<"After Merging : "<<endl;
+    printarr(arr3,size);
+    sorting(arr3,size);
+    cout<<"After Sorting : "<<endl;
+    printarr(arr3,size);
 }
