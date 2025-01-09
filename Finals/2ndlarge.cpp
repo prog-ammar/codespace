5e39a8682103fe167679e97e0fffb67d64744203
prog-ammar <ammarofc2005@gmail.com>
2025-01-09 19:42:11 +0500

    updated

--- Finals/2ndlarge.cpp
+++ Finals/2ndlarge.cpp
@@ -5,22 +5,20 @@ using namespace std;
 void second(int p[],int n)
 {
     int largest=p[0];
-    int seclr=p[0];
-    for(int i=1;i<n;i++)
+    int seclr=p[1];
+    for(int i=0;i<n;i++)
     {
         if(largest<p[i])
         {
+            seclr=largest;
             largest=p[i];
         }
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(seclr<p[i])
+        if(p[i]!=largest)
         {
-            if(p[i]!=largest)
-            {
-              seclr=p[i];
-            }
+          if(seclr<p[i])
+          {
+            seclr=p[i];
+          }
         }
     }
     cout<<largest<<" "<<seclr<<"\n";
