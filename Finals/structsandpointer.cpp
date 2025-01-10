ccc88c39fc307b90620a37a6c29ec7e012366929
prog-ammar <ammarofc2005@gmail.com>
2025-01-10 12:59:07 +0500

    updated

--- Finals/structsandpointer.cpp
+++ Finals/structsandpointer.cpp
@@ -10,11 +10,31 @@ struct Student
 
 int main()
 {
-    Student* s=new Student();
-    cout<<"Enter Name and Marks : ";
-    cin>>s->name>>s->marks;
+    // Student* s=new Student();
 
-    cout<<"Student Data \n";
-    cout<<s->name<<" "<<s->marks;
+    // cout<<"Enter Name and Marks : ";
+    // cin>>s->name>>s->marks;
+
+    // cout<<"Student Data \n";
+    // cout<<s->name<<" "<<s->marks;
+    int n;
+    cout<<"Enter no of Students : ";
+    cin>>n;
+    Student* s=new Student[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter Student Name : ";
+        cin.ignore();
+        getline(cin,s[i].name);
+        cout<<"Enter Student Marks : ";
+        cin>>s[i].marks;
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Name : "<<s[i].name<<endl;
+        cout<<"Marks : "<<s[i].marks;
+        cout<<endl;
+    }
     
 }
