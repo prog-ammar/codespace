e3facb806735f89e5aebcd6fbeb5fab87501c0a6
prog-ammar <ammarofc2005@gmail.com>
2025-02-07 17:54:37 +0500

    added

--- oops/class.cpp
+++ oops/class.cpp
@@ -8,11 +8,11 @@ class Car
 
     int manuYear;//Member Varaiables
     string name;
-    string brand;
+    string brand; //static variable is shared amoung all the objects in an class and any where its change it change for all objects
 
     public:
-    
-    Car(): name(""), brand(""), manuYear(0) {} //Constructor Automatically Called When Object is Created
+    static int totalCount;
+    Car(): name(""), brand(""), manuYear(0){totalCount++;} //Constructor Automatically Called When Object is Created
 
     void set(string carName,string carBrand,int Year)//Member Functions //Setter Function
     {
@@ -26,16 +26,25 @@ class Car
         cout<<"Name of Car : "<<name<<"\nBrand of Car : "<<brand<<"\nYear of Manufacturing : "<<manuYear<<endl;
     }
 
+    static int getNoofCars()
+    {
+        return totalCount;
+    }
+
     ~Car()//Destructor Automatically called at end 
     {
         cout<<"Car has been Destroyed \n";
+        totalCount--;
     }
 };
+
+int Car::totalCount=0;
 
 int main()
 {
   Car a;
   a.set("City","Honda",2005);
+  cout<<"Total Cars : "<<Car::getNoofCars<<"\n";
   a.get();
   return 0;
 }
